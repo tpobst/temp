2b2e5d9f8bd84a87bca7379862ba0c577ef8a846
tpobst <tim.pobst@gmail.com>
2015-02-03 13:47:32 -0500

    Ex41

---
+++ Ex41.py
@@ -0,0 +1,77 @@
+## Animal is-a object (yes, sort of confusing) look at the extra credit
+class Animal(object):
+	pass
+	
+## Dog is-a Animal
+class Dog(Animal):
+
+	def __init__(self, name):
+		## self has-a name
+		self.name = name
+		
+## Cat is-a Animal
+class Cat(Animal):
+
+	def __init__(self, name):
+		##self has-a name
+		self.name = name
+		
+## Person is-a object
+class Person(object):
+	
+	def __init__(self,name):
+		## self has-a name
+		self.name = name
+		
+		## self has-a pet. Person has-a pet of some kind
+		self.pet = None
+		
+## Employee is-a person
+class Employee(Person):
+
+	def __init__(self, name, salary):
+		##?? hmm what is this strange magic?
+		super(Employee, self).__init__(name)
+		## self has-a salary
+		self.salary = salary
+		
+##?? is
+class Fish(object):
+	pass
+	
+##?? is
+class Salmon(Fish):
+	pass
+	
+##?? is
+class Halibut(Fish):
+	pass
+	
+	
+##rover is-a Dog
+rover = Dog("Rover")
+
+##?? is
+satan = Cat("Satan")
+
+##?? is
+mary = Person("Mary")
+
+##?? has
+mary.pet = satan
+
+##?? is
+frank = Employee("Frank", 120000)
+
+##?? has
+frank.pet = rover
+
+##?? is
+flipper = Fish()
+
+##?? is
+crouse = Salmon()
+
+##?? is
+harry = Halibut()
+	
